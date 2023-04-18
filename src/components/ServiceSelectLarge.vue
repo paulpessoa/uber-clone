aae7bd102f228e6e5016e2991f99c236aa9035eb
Paul Pessoa <paulmspessoa@gmail.com>
2023-04-18 16:23:08 -0300

    small component and import images

--- src/components/ServiceSelectLarge.vue
+++ src/components/ServiceSelectLarge.vue
@@ -1,18 +1,52 @@
 <template>
   <div id="ServiceSelectLarge">
     <div class="w-full rounded-2xl bg-custom-color custom-height">
+      
+      <div v-if="promo" class="w-full relative z-10">
+        <span
+          class="
+            absolute
+            top-1/2
+            left-1/2
+            -translate-x-1/2
+            -translate-y-1/2
+            bg-green-700
+            p-0.5
+            px-2
+            text-gray-100
+            rounded-3xl
+            text-xs
+          ">
+          Promo
+      </span>
+      </div>
+
       <div class="flex justify-between p-2">
         <div class="text-lg ml-2 text-gray-900 mt-14">
-          Ride
+          {{text}}
         </div>
         <div class="mt-3 mr-1">
-          <img width="70" src="img/uber/ride.png" alt="ride" />
+          <img :width="imageWidth" :src="`img/uber/${image}.png`" :alt="image" />
         </div> 
       </div>
     </div>
 
   </div>
 </template>
+
+<script setup>
+import { toRefs, defineProps } from 'vue'
+const props = defineProps({
+    text: String,
+    imageWidth: String,
+    image: String,
+    promo: {
+      type: Boolean,
+      default: false},
+  })
+  const { text, imageWidth, image, promo } = toRefs(props)
+
+</script>
 
 
 <style lang="scss" scoped>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,7 +2,14 @@
   <div id="Home" class="flex flex-col h-screen">
     <div class="m-3 mb-auto">
       <div class="grid grid-cols-2 gap-3 my-3">
-        <ServiceSelectLarge />
+        <ServiceSelectLarge text="Ride" imageWidth="70" image="Ride"/>
+        <ServiceSelectLarge text="Food" imageWidth="70" image="Food" :promo="true"/>
+      </div>
+      <div class="grid grid-cols-4 gap-3 my-3">
+        <ServiceSelectSmall text="2 Wheels" image="2Wheels"/>
+        <ServiceSelectSmall text="Transit" image="transit"/>
+        <ServiceSelectSmall text="Reserve" image="Reserve"/>
+        <ServiceSelectSmall text="Travel" image="travel"/>
       </div>
     </div>
   </div>
@@ -11,6 +18,7 @@
 <script setup>
 
 import ServiceSelectLarge from '@/components/ServiceSelectLarge.vue';
+import ServiceSelectSmall from '@/components/ServiceSelectSmall.vue';
 
 
 </script>
