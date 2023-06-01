445d72013ff91d1acbdfe4e2658aa4f6871a0cdc
Paul Pessoa <paulmspessoa@gmail.com>
2023-06-01 11:44:42 -0300

    install and config pinia

--- src/main.js
+++ src/main.js
@@ -3,5 +3,18 @@ import App from './App.vue'
 import './registerServiceWorker'
 import router from './router'
 import './assets/tailwind.css'
+import axios from 'axios'
 
-createApp(App).use(router).mount('#app')
+axios.defaults.baseURL = process.env.VUE_APP_API_URL
+
+import { createPinia } from 'pinia'
+import { piniaPluginPersistedState } from 'pinia-plugin-persistedstate'
+
+const pinia = createPinia()
+pinia.use(piniaPluginPersistedState)
+
+const app = createApp(App)
+
+app.use(router)
+app.use(pinia)
+.mount('#app')
